f596f939c8439d06648f4565853b42fa2649dd19
Vitaliy Zlobin <zlovian@yandex.ru>
2024-10-15 08:47:21 +0700

    type method canMove changed

--- src/Piece.java
+++ src/Piece.java
@@ -3,6 +3,8 @@ public abstract class Piece {
     private final char pieceSymbol;
     public boolean isDidntMove = true;
 
+    public abstract boolean canMove(Piece[][] board, int line, int column, int toLine, int toColumn);
+
     public Piece(PieceColor pieceColor, char whiteSymbol, char blackSymbol) {
         this.pieceColor = pieceColor;
         this.pieceSymbol = pieceColor.equals(PieceColor.white) ? whiteSymbol : blackSymbol;
@@ -10,10 +12,6 @@ public abstract class Piece {
 
     public PieceColor getColor() {
         return pieceColor;
-    }
-
-    public boolean canMove(Piece[][] board, int line, int column, int toLine, int toColumn) {
-        return true;
     }
 
     @Override
